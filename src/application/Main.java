0f38d86d40c26ae7d5c6e49697e04950bb4fe3ae
Skennymon <77165403+Skennymon@users.noreply.github.com>
2024-10-18 18:49:38 -0700

    Replaced textfield with date picker in new account page

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.time.LocalDate;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.HPos;
@@ -8,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -38,15 +41,19 @@ public class Main extends Application {
 			root.add(home, 1, 1);
 			
 			//buttons for home page
-			Button btnTop = new Button("Add new Account");
-			Button btnMid = new Button("Add new Transaction");
-			Button btnBottom = new Button("Schedule Transaction");
-			root.add(btnTop, 1, 2);
-			root.add(btnMid, 1, 3);
-			root.add(btnBottom, 1, 4);
+			Button newAccountButton = new Button("Add new Account");
+			Button newTransactionButton = new Button("Add new Transaction");
+			Button scheduleTransactionButton = new Button("Schedule Transaction");
+			Button showAccountButton = new Button("View Accounts");
+			Button showTransactionsButton = new Button("View Transactions");
+			root.add(newAccountButton, 1, 2);
+			root.add(newTransactionButton, 1, 3);
+			root.add(scheduleTransactionButton, 1, 4);
+			root.add(showAccountButton, 1, 5);
+			root.add(showTransactionsButton, 1, 6);
 			
 			//When you click "Add new Account" it runs this so that the program switches scene the the new account page
-			btnTop.setOnAction(e -> primaryStage.setScene(addNewAccountScene)); 
+			newAccountButton.setOnAction(e -> primaryStage.setScene(addNewAccountScene)); 
 			
 			//Add New Account Scene customization
 			newAcc.setPadding(new Insets(20, 20, 20, 20));
@@ -58,9 +65,16 @@ public class Main extends Application {
 			accountNameInput.setPromptText("Input Account Name");
 			GridPane.setConstraints(accountNameInput, 0, 1);
 			
+			//-------
+			/*
 			TextField accountOpeningDateInput = new TextField();
 			accountOpeningDateInput.setPromptText("Input Account Opening Date");
 			GridPane.setConstraints(accountOpeningDateInput, 0, 2);
+			*/
+			DatePicker dp = new DatePicker();
+			dp.setValue(LocalDate.now());
+			GridPane.setConstraints(dp, 0, 2);
+			//-------
 			
 			TextField accountOpeningBalance = new TextField();
 			accountOpeningBalance.setPromptText("Input Account Opening Balance");
@@ -74,11 +88,11 @@ public class Main extends Application {
 			
 			newAcc.setVgap(50);
 			
-			newAcc.getChildren().addAll(addNewAccountText, accountNameInput, accountOpeningDateInput, accountOpeningBalance, confirmBtn, cancelBtn); 
+			newAcc.getChildren().addAll(addNewAccountText, accountNameInput, dp, accountOpeningBalance, confirmBtn, cancelBtn); 
 			
 			//if the user presses confirm without entering all the fields, then it prompts the user to enter all the fields
 			confirmBtn.setOnAction(e -> {
-				if(accountNameInput.getText().isEmpty() || accountOpeningDateInput.getText().isEmpty() || accountOpeningBalance.getText().isEmpty()) {
+				if(accountNameInput.getText().isEmpty() || accountOpeningBalance.getText().isEmpty()) {
 					Text error = new Text("You must enter in all the fields");
 					GridPane.setConstraints(error, 0, 5);
 					newAcc.getChildren().addAll(error);
