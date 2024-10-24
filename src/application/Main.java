d30f7b4bee6e219145ec194bc75d25b1ddd8ae42
Skennymon <77165403+Skennymon@users.noreply.github.com>
2024-10-23 21:56:46 -0700

    Removed unecessary imports

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,10 @@
 package application;
 	
-import java.time.LocalDate;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-
 
 public class Main extends Application {
 	
