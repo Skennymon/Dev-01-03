19b4a6f8932ba73945c5bc14ecfba52daed51667
Skennymon <77165403+Skennymon@users.noreply.github.com>
2024-10-23 21:55:03 -0700

    Made error prompt
    If user doesn't enter all of the fields in new account page, it gives the user an alert.

--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -1,12 +1,19 @@
 package application;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SceneController {
@@ -14,6 +21,13 @@ public class SceneController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	
+	@FXML 
+	private TextField accountNameInput;
+	@FXML
+	private TextField accountOpeningBalanceInput;
+	@FXML
+	private DatePicker accountOpeningInput;
 	
 	public void switchtoHomeScene(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
@@ -30,6 +44,26 @@ public class SceneController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void promptUserToEnterAllFields(ActionEvent event) throws IOException {
+		
+		if(accountNameInput.getText().isEmpty() || accountOpeningBalanceInput.getText().isEmpty() || accountOpeningInput.getValue() == null) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Alert!");
+			alert.setContentText("Please enter in all fields");
+			Optional<ButtonType> result = alert.showAndWait();
+		}
+		else {
+			Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		
+	}
+	
+	
 	
 	
 }
