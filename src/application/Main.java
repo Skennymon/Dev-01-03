5bc4e00ea7fccffc25b5c7775c2229787b3c65ea
Skennymon <77165403+Skennymon@users.noreply.github.com>
2024-10-23 21:03:30 -0700

    Implemented Scene Builder
    Rewrote the code so that we can use SceneBuilder and make our life easier.

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -27,87 +29,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			GridPane root = new GridPane();
-			GridPane newAcc = new GridPane();
-			Scene homeScene = new Scene(root, 1024, 768);
-			Scene addNewAccountScene = new Scene(newAcc, 1024, 768);
+			Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
 			
-			root.setPadding(new Insets(20, 20, 20, 20));
-			root.setVgap(50);
 			
-			//"Home" text
-			Text home = new Text("Home");
-			home.setFont(Font.font("Verdana", 50));
-			root.add(home, 1, 1);
 			
-			//buttons for home page
-			Button newAccountButton = new Button("Add new Account");
-			Button newTransactionButton = new Button("Add new Transaction");
-			Button scheduleTransactionButton = new Button("Schedule Transaction");
-			Button showAccountButton = new Button("View Accounts");
-			Button showTransactionsButton = new Button("View Transactions");
-			root.add(newAccountButton, 1, 2);
-			root.add(newTransactionButton, 1, 3);
-			root.add(scheduleTransactionButton, 1, 4);
-			root.add(showAccountButton, 1, 5);
-			root.add(showTransactionsButton, 1, 6);
-			
-			//When you click "Add new Account" it runs this so that the program switches scene the the new account page
-			newAccountButton.setOnAction(e -> primaryStage.setScene(addNewAccountScene)); 
-			
-			//Add New Account Scene customization
-			newAcc.setPadding(new Insets(20, 20, 20, 20));
-			Text addNewAccountText = new Text("Add New Account");
-			addNewAccountText.setFont(Font.font("Verdana", 50));
-			GridPane.setConstraints(addNewAccountText, 0, 0);
-			
-			TextField accountNameInput = new TextField();
-			accountNameInput.setPromptText("Input Account Name");
-			GridPane.setConstraints(accountNameInput, 0, 1);
-			
-			//-------
-			/*
-			TextField accountOpeningDateInput = new TextField();
-			accountOpeningDateInput.setPromptText("Input Account Opening Date");
-			GridPane.setConstraints(accountOpeningDateInput, 0, 2);
-			*/
-			DatePicker dp = new DatePicker();
-			dp.setValue(LocalDate.now());
-			GridPane.setConstraints(dp, 0, 2);
-			//-------
-			
-			TextField accountOpeningBalance = new TextField();
-			accountOpeningBalance.setPromptText("Input Account Opening Balance");
-			GridPane.setConstraints(accountOpeningBalance, 0, 3);
-			
-			Button cancelBtn = new Button("Cancel");
-			GridPane.setConstraints(cancelBtn, 0, 4);
-			
-			Button confirmBtn = new Button("Confirm");
-			GridPane.setConstraints(confirmBtn, 1, 4);
-			
-			newAcc.setVgap(50);
-			
-			newAcc.getChildren().addAll(addNewAccountText, accountNameInput, dp, accountOpeningBalance, confirmBtn, cancelBtn); 
-			
-			//if the user presses confirm without entering all the fields, then it prompts the user to enter all the fields
-			confirmBtn.setOnAction(e -> {
-				if(accountNameInput.getText().isEmpty() || accountOpeningBalance.getText().isEmpty()) {
-					Text error = new Text("You must enter in all the fields");
-					GridPane.setConstraints(error, 0, 5);
-					newAcc.getChildren().addAll(error);
-				}
-				else {
-					primaryStage.setScene(homeScene);
-				}
-			});
-			cancelBtn.setOnAction(e -> primaryStage.setScene(homeScene));
-			
-			//final initialization pls don't touch
-			primaryStage.setTitle("Finance Tracker");
-			primaryStage.setScene(homeScene);
-			primaryStage.show(); 
-		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/application/SceneController.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneController {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchtoHomeScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToNewAccountScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("NewAccountScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
+}
