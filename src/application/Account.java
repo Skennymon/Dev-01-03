abb8d558d73c745aedbce9ecbede35f270fb6448
Skennymon <77165403+Skennymon@users.noreply.github.com>
2024-10-25 13:05:17 -0700

    Added View Account Scene

---
+++ src/application/Account.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.scene.control.DatePicker;
+
+public class Account {
+	
+	private String accountName;
+	private DatePicker accountOpeningDate;
+	private int accountOpeningBalance;
+	
+	public void setAccountName() {
+		
+	}
+	
+	public void setAccountOpeningDate() {
+		
+	}
+	
+	public void setAccountOpeningBalance() {
+		
+	}
+	
+	
+}
--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -45,8 +45,17 @@ public class SceneController {
 		stage.show();
 	}
 	
+	public void switchToViewAccountScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ViewAccountScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
 	public void promptUserToEnterAllFields(ActionEvent event) throws IOException {
 		
+		//if the user hasn't entered all the required fields, it gives an alert to the user that they need to do that
 		if(accountNameInput.getText().isEmpty() || accountOpeningBalanceInput.getText().isEmpty() || accountOpeningInput.getValue() == null) {
 			Alert alert = new Alert(Alert.AlertType.ERROR);
 			alert.setTitle("Alert!");
