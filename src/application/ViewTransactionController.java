7c445885d99211edc0b84dba9abb1495ecf9fa5b
Skennymon <77165403+Skennymon@users.noreply.github.com>
2024-11-12 22:53:39 -0800

    Added back to home function in view transaction scene

--- src/application/ViewTransactionController.java
+++ src/application/ViewTransactionController.java
@@ -10,13 +10,19 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 public class ViewTransactionController implements Initializable {
 	
@@ -34,6 +40,10 @@ public class ViewTransactionController implements Initializable {
 	private TableColumn<Transaction, String> paymentAmountCol;
 	@FXML
 	private TableColumn<Transaction, String> depositAmountCol;
+	
+	private Parent root;
+	private Scene scene;
+	private Stage stage;
 	
 	public void initialize(URL url, ResourceBundle bundle) {
 		ObservableList<Transaction> accountList = FXCollections.observableArrayList();
@@ -77,6 +87,14 @@ public class ViewTransactionController implements Initializable {
 			e.printStackTrace();
 		}
 		return transactionList;
+	}
+	
+	public void switchToHomeScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 }
